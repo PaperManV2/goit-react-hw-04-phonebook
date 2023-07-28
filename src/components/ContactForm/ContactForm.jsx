29ceb536f8a5b0d70da06a527a43ec8ec6212883
Jonasz Janicki <34692088+PaperManV2@users.noreply.github.com>
2023-07-28 19:38:33 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -38,7 +38,7 @@ export const ContactForm = ({ onAddContact }) => {
         type="text"
         name="name"
         placeholder="Contact name"
-        pattern="^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+([-'\\s][a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+)*$"
+        pattern="^[A-Za-z.'\\- \]+$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         value={name}
@@ -49,7 +49,7 @@ export const ContactForm = ({ onAddContact }) => {
         type="text"
         name="number"
         placeholder="Number"
-        pattern="^\\+?\\d{1,4}[-.\\s]?\\(?\d{1,3}\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
+        pattern="^\\+?\\d{1,4}?\\s?\\(?\\d{1,4}?\\)?\\s?\\d{1,4}\\s?\\d{1,4}\\s?\\d{1,9}$"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
         value={number}
